e81bfb84d069806c340b95e46a8b057a0a5d48c5
Petr <petr_mail@mail.ru>
2022-10-20 19:27:49 +0300

    первое домашнее

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// является ли число четным
+
+Console.WriteLine("Введите  число");
+int num = Convert.ToInt32(Console.ReadLine());
+int OST = num % 2;
+if (OST==0)
+{
+    Console.WriteLine($"число {num} является  четным");
+}
+
+else
+Console.WriteLine($"число {num}  не является  четным");
