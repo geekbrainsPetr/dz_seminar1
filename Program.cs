1ec49d2a37649e268da5ebf11672f836ad51da13
Petr <petr_mail@mail.ru>
2022-10-20 21:50:42 +0300

    дз первый семинар

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// является ли число четным
 
+/*
 Console.WriteLine("Введите  число");
 int num = Convert.ToInt32(Console.ReadLine());
 int OST = num % 2;
@@ -10,3 +11,91 @@ if (OST==0)
 
 else
 Console.WriteLine($"число {num}  не является  четным");
+*/
+
+
+/*
+// Определение большего и меньшего числа3
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+ if (num1 > num2)
+ {
+     Console.WriteLine($"число {num1} больше {num2}");
+ }
+
+ else 
+ {
+     Console.WriteLine($"число {num2} больше {num1}");
+ }
+ 
+ */
+
+
+
+ // задание сформулировано криво. В тексте "выдает какое больше, а какое меньше". В примере - выводится только максимальное!
+ // второй вариант
+
+
+ /*
+
+ Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+ if (num1 > num2)
+ {
+     Console.WriteLine($" Максимальное число число {num1}");
+ }
+
+ else 
+ {
+     Console.WriteLine($" Максимальное число {num2} ");
+ }
+ */
+
+
+/*
+ //Максимальное из трех чисел
+
+ Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+ {
+    if (num1 > num3)
+
+    {
+     Console.WriteLine($"Максимальное число {num1}");
+    }
+    else
+    {
+        Console.WriteLine($"Максимальное число {num3}");
+    }
+ }
+ else if (num2 > num3)
+ {
+    Console.WriteLine($"Максимальное число {num2} ");
+
+ }
+ else
+ {
+    Console.WriteLine($"Максимальное число {num3} ");
+ }
+ */
+
+// все четные числа в диапазоне 0 - N
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1
+while (i <= num)
+
+
+
